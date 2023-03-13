0170a5eef5cb6e8fbe7695882b32a6eb4509fdd3
Patrice Tekeda <patricetekeda@gmail.com>
2023-03-13 08:47:14 -0400

    new update

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,15 +1,14 @@
 //// Service.js file:  holds functions that make all the CRUD transactions for one table ////
-const connection = require("../db/connection");
+const knex = require("../db/connection");
 
+/** creates a new reservation (row) */
 function create(reservation) {
-  const db = connection();
-  return db("reservations").insert(reservation).returning("*");
+  return knex("reservations").insert(reservation).returning("*");
 }
 
 /** reads the data (row) with the given 'reservation_id'. */
 function read(reservation_id) {
-  const db = connection();
-  return db("reservations")
+  return knex("reservations")
     .select("*")
     .where({ reservation_id: reservation_id })
     .first();
@@ -17,16 +16,14 @@ function read(reservation_id) {
 
 /** updates reservation with the given reservation_id. */
 function update(reservation_id, status) {
-  const db = connection();
-  return db("reservations")
+  return knex("reservations")
     .where({ reservation_id: reservation_id })
     .update({ status: status });
 }
 
 /** edits reservation with the given reservation_id. */
 function edit(reservation_id, reservation) {
-  const db = connection();
-  return db("reservations")
+  return knex("reservations")
     .where({ reservation_id: reservation_id })
     .update({ ...reservation })
     .returning("*");
@@ -34,22 +31,20 @@ function edit(reservation_id, reservation) {
 
 /** lists all reservations with the given date or mobile number. */
 function list(date, mobile_number) {
-  const db = connection();
   if (date) {
-    return db("reservations")
+    return knex("reservations")
       .select("*")
       .where({ reservation_date: date })
       .orderBy("reservation_time", "asc");
   }
 
   if (mobile_number) {
-    return db("reservations")
+    return knex("reservations")
       .select("*")
-      .where("mobile_number", "regexp", `^${mobile_number}`)
-      .orderBy("reservation_time", "asc");
+      .where("mobile_number", "like", `${mobile_number}%`);
   }
 
-  return db("reservations").select("*");
+  return knex("reservations").select("*");
 }
 
 module.exports = {
--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -1,50 +1,43 @@
-const connection = require("../db/connection");
+const knex = require("../db/connection");
 
 /** creates a new table (row) */
 function create(table) {
-  const db = connection();
-  return db("tables").insert(table).returning("*");
+  return knex("tables").insert(table).returning("*");
 }
 
 /** reads a table given the table_id */
 function read(table_id) {
-  const db = connection();
-  return db("tables").select("*").where({ table_id: table_id }).first();
+  return knex("tables").select("*").where({ table_id: table_id }).first();
 }
 
 /** updates reservation status given the reservation_id */
 function updateReservation(reservation_id, status) {
-  const db = connection();
-  return db("reservations")
+  return knex("reservations")
     .where({ reservation_id: reservation_id })
     .update({ status: status });
 }
 
 /** lists all tables. */
 function list() {
-  const db = connection();
-  return db("tables").select("*").orderBy("table_name");
+  return knex("tables").select("*").orderBy("table_name");
 }
 
 /** reads reservation given the reservation_id. */
 function readReservation(reservation_id) {
-  const db = connection();
-  return db("reservations")
+  return knex("reservations")
     .select("*")
     .where({ reservation_id: reservation_id })
     .first();
 }
 
 function occupy(table_id, reservation_id) {
-  const db = connection();
-  return db("tables")
+  return knex("tables")
     .where({ table_id: table_id })
     .update({ reservation_id: reservation_id, status: "occupied" });
 }
 
 function free(table_id) {
-  const db = connection();
-  return db("tables")
+  return knex("tables")
     .where({ table_id: table_id })
     .update({ reservation_id: null, status: "free" });
 }
